7279b7e15ee5506dffe38b67d798f3ad9563bf1a
AndrewME89 <137775398+AndrewME89@users.noreply.github.com>
2024-07-22 06:42:26 +1000

    Add files via upload

---
+++ subscriberform.php
@@ -0,0 +1,6 @@
+<?php
+if($_POST[“message”]) {
+    mail(“AndrewMEdwards1989@gmail.com”, “Here is the subject line”,
+    $_POST[“insert your message here”]. “From: an@email.address”);
+    }
+    ?>
